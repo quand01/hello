4bc7f9ac73a431029e9e4170aef99ad77bec088a
quand01 <89498237+quand01@users.noreply.github.com>
2021-08-30 14:59:21 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,16 +1,16 @@
-   /**
-    *Author: Quan Do 
+  /**
+    *Author: Quan Do
     * Date: 2021/08/24
-    * 
-    * A simple hello world program in C 
-    * 
+    *
+    * A simple hello world program in C
+    *
     */
-    #include <stdlib.h> 
-    #include <stdio.h>
-    
+#include <stdlib.h>
+#include <stdio.h>
+
     int main(int argc, char **argv) {
-        
-        printf("Hello World!\n");
-        
+
+        printf("Quan Do!\n");
+
         return 0;
-    }
+}
