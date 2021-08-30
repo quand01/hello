17253f7c2b8cb4895503abd3b51ad8a0cb1dd844
Quan Do <qdo3@huskers.unl.edu>
2021-08-30 19:35:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+   /**
+    *Author: Quan Do 
+    * Date: 2021/08/24
+    * 
+    * A simple hello world program in C 
+    * 
+    */
+    #include <stdlib.h> 
+    #include <stdio.h>
+    
+    int main(int argc, char **argv) {
+        
+        printf("Hello World!\n");
+        
+        return 0;
+    }
